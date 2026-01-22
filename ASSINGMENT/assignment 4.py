2ca888075755940588fd5474ba92bbf3bfec6e40
quandao123 <95832846+quandao123@users.noreply.github.com>
2026-01-22 15:06:57 +0700

    Update assignment 4.py

--- ASSINGMENT/assignment 4.py
+++ ASSINGMENT/assignment 4.py
@@ -1,9 +1,1 @@
-num1 = int(input ("Enter the first number: "))
-num2 = int(input ("Enter the second number: "))
-num3 = int(input ("Enter the third number: "))
-sum = num1 + num2 + num3
-print(f"The sum of {num1} and {num2} and {num3} is {sum}")
-product = num1 * num2 * num3
-print(f"The product of {num1} and {num2} and {num3} is {product}")
-average = (num1 + num2 + num3) / 3
-print(f"The average of {num1} and {num2} and {num3} is {average}")
+
