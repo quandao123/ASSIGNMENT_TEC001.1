def86ade6a4856d42cee0fd2814c21d356b3fae8
quandao123 <95832846+quandao123@users.noreply.github.com>
2026-01-22 15:06:43 +0700

    Update assignment 3.py

--- ASSINGMENT/assignment 3.py
+++ ASSINGMENT/assignment 3.py
@@ -1,9 +1,1 @@
-length = input("Enter the length of the rectangle: ")
-width = input("Enter the width of the rectangle: ")
-length = float(length)
-width = float(width)
-area = length * width
-print(f"The area of the rectangle is {area}")
-area = float(area)
-perimeter = length * 2 + width * 2
-print(f"The perimeter of the rectangle is {perimeter}")
+
