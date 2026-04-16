e4cf18f3a0911358ea1b7f70722a6291894728cf
quan <daominhquan08102007@gmail.com>
2026-04-16 12:33:12 +0700

    s1

--- assignment10/assignment10.py
+++ assignment10/assignment10.py
@@ -4,7 +4,7 @@ def get_chuck_norris_joke():
     url = "https://api.chucknorris.io/jokes/random"
 
     try:
-        response = requests.get(url)# Gửi yêu cầu GET tới API
+        response = requests.get(url)# Gửi yêu cầu get tới API
 
         # Kiểm tra nếu yêu cầu thành công (status code 200 là good chạy ngon)
         if response.status_code == 200:
@@ -39,6 +39,7 @@ if response.status_code == 200:
     print(f"Nhiệt độ: {data['main']['temp']}°C")
 else:
     print("Lỗi rồi! Có thể Key chưa kích hoạt hoặc sai tên thành phố.")
+
 # #-------------------------3----------------
 from flask import Flask, json
 app = Flask(__name__)
@@ -62,36 +63,28 @@ if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=5000)
 # #-----------------------4-----------------------4
 from flask import Flask, json
+import json
 
 app = Flask(__name__)
-@app.route('/airport/<string:icao>')
+
+
+with open('airports.json', 'r') as f:
+    airports = json.load(f)
+
+@app.route('/airport/<icao>')
 def get_airport(icao):
-    result = find_airport_in_file(icao)
+    icao = icao.upper()
 
-    if result == "FILE_ERROR":
-        return json.dumps({"message": "Server file error"})
+    if icao not in airports:
+        return json.dumps({"error": f"Airport '{icao}' not found"})
 
-    if result:
-        return json.dumps(result), 200
-    else:
-        return json.dumps({"message": "Airport not found"})
+    airport = airports[icao]
+    return json.dumps({
+        "icao": icao,
+        "name": airport["name"],
+        "city": airport["city"],
+        "country": airport["country"]
+    })
 
-def find_airport_in_file(icao_to_find):
-    try:
-        with open('airports.txt', 'r', encoding='utf-8') as f:
-            for line in f:
-                parts = line.strip().split(',')
-
-                if len(parts) == 4 and parts[0].upper() == icao_to_find.upper():
-                    return {
-                        "icao": parts[0],
-                        "name": parts[1],
-                        "city": parts[2],
-                        "country": parts[3]
-                    }
-        return None
-    except FileNotFoundError:
-        print("Lỗi: Không tìm thấy file airports.txt")
-        return "FILE_ERROR"
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=5000)
